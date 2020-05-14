d14d72dc92de455adb74f5dbc3306df3dae888a7
Anthony <awrodriguezt@gmail.com>
2020-05-14 14:27:54 -0600

    Basics for enemy creation and maintenance

--- routes/api/admin/enemies/enemies.js
+++ routes/api/admin/enemies/enemies.js
@@ -16,8 +16,49 @@ function initEnemies (db) {
     });
 
     router.post('/newEnemy', (req, res)=>{
-        var data = req.body;
+        var a = req.body.WP.toLowerCase();
+        var heal =  parseInt(req.body.heal);
+        var attack = parseInt(req.body.atk);
+        var b = a.split(" ");
+        var WP = {};
+        if(b.length===1){
+            WP = b[0].charAt(0).toUpperCase()+b[0].slice(1);
+        }else{
+            WP = b[0].charAt(0).toUpperCase()+b[0].slice(1)+" "+b[1].charAt(0).toUpperCase()+b[1].slice(1);
+        }
+        var data = {
+            "weapon": WP,
+            "health": heal,
+            "attack": attack,
+            ...req.body
+        }
         enemiesModel.newEnemy(data, (err, enemy)=>{
+            if(err){
+                console.log(err);
+                return res.status(500).json(err);
+            }
+            return res.status(200).json(enemy);
+        });
+    });
+
+    router.put('/updateEnemy', (req, res)=>{
+        var heal =  parseInt(req.body.heal);
+        var attack = parseInt(req.body.atk);
+        var a = req.body.WP.toLowerCase();
+        var b = a.split(" ");
+        var WP = {};
+        if(b.length===1){
+            WP = b[0].charAt(0).toUpperCase()+b[0].slice(1);
+        }else{
+            WP = b[0].charAt(0).toUpperCase()+b[0].slice(1)+" "+b[1].charAt(0).toUpperCase()+b[1].slice(1);
+        }
+        var data = {
+            "weapon": WP,
+            "health": heal,
+            "attack": attack,
+            ...req.body
+        }
+        enemiesModel.updateEnemy(data, (err, enemy)=>{
             if(err){
                 console.log(err);
                 return res.status(500).json(err);
--- routes/api/admin/enemies/enemies.model.js
+++ routes/api/admin/enemies/enemies.model.js
@@ -3,6 +3,7 @@ var ObjectID = require('mongodb').ObjectID;
 module.exports = (db) =>{
     enemiesModel = {};
     var enemiesCollection = db.collection("enemies");
+    var objectsInvCollection = db.collection("objectsInv");
 
     var enemyTemplate = {
         enemyName: "",
@@ -17,9 +18,74 @@ module.exports = (db) =>{
     };
 
     enemiesModel.newEnemy = (data, handler)=>{
-        var {name, health, weapon, atk} = data;
+        var {name, health, weapon, attack, desc} = data;
+        objectsInvCollection.find({}).toArray((err, objs)=>{
+            if(err){
+                console.log(err);
+                return handler(err, null);
+            }
+            var WP = {};
+            for(var a=0;a<objs.length;a++){
+                if(objs[a].objectName===weapon){
+                    WP = objs[a];
+                }
+            }
+            var enemy = Object.assign(
+                {},
+                enemyTemplate,
+                {
+                    enemyName: name,
+                    enemyATK: attack,
+                    enemyHealth: health,
+                    enemyWeapon: WP,
+                    enemyDesc: desc,
+                }
+            );
+            enemiesCollection.insertOne(enemy, (err, rslt)=>{
+                if(err){
+                    console.log(err);
+                    return handler(err, null);
+                }
+                return handler(null, rslt.ops);
+            });    
+        });
+    };
 
-    }
+    enemiesModel.updateEnemy = (data, handler)=>{
+        var {nameOG, nameNew, health, weapon, attack, desc} = data;
+        objectsInvCollection.find({}).toArray((err, objs)=>{
+            if(err){
+                console.log(err);
+                return handler(err, null);
+            }
+            var WP = {};
+            for(var a=0;a<objs.length;a++){
+                if(objs[a].objectName===weapon){
+                    WP = objs[a];
+                }
+            }
+            var query = {"enemyName": nameOG};
+            var updateCommand = {
+                $set:{
+                    "enemyName": nameNew,
+                    "enemyATK": attack,
+                    "enemyHealth": health,
+                    "enemyWeapon": WP,
+                    "enemyDesc": desc
+                }
+            }
+            enemiesCollection.findOneAndUpdate(
+                query,
+                updateCommand,
+                (err, upd)=>{
+                if(err){
+                    console.log(err);
+                    return handler(err, null);
+                }
+                return handler(null,upd);
+            });    
+        });
+    };
 
     return enemiesModel;
 }
